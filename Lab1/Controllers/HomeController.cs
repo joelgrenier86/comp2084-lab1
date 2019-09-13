75da5e21c9ab57d925c4e1ba32b3a69033f10ec0
100037633 <joelgrenier@hotmail.com>
2019-09-12 20:14:09 -0400

    Uploading lab 1 to git

--- Lab1/Controllers/HomeController.cs
+++ Lab1/Controllers/HomeController.cs
@@ -19,7 +19,10 @@ namespace Lab1.Controllers
         {
             return View();
         }
-
+        public IActionResult About()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
